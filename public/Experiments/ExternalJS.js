a91434fb56030b784acf993f12dddb2893e97f08
GovindaBachani <govinda.bachanis@gmail.com>
2015-10-02 15:16:29 -0400

    assignment start

---
+++ public/Experiments/ExternalJS.js
@@ -0,0 +1,13 @@
+﻿function changeBulbColor() {
+    var bulb = document.getElementById("bulb");
+    if (bulb.src.match("pic_bulboff")) {
+        bulb.src = "pic_bulbon.gif";
+    }
+    else if (bulb.src.match("pic_bulbon")) {
+        bulb.src = "pic_bulboff.gif";
+    }
+}
+
+function myFunction() {
+    alert(5 + 6);
+}
