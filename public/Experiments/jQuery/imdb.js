114aaabb81e21ffb96abc6b1c569bad554f51663
GovindaBachani <govinda.bachanis@gmail.com>
2015-10-09 01:46:12 -0400

    experimenting with soccer API

---
+++ public/Experiments/jQuery/imdb.js
@@ -0,0 +1,79 @@
+﻿(function(){
+    $(init);
+
+    function init(){
+        $("#searchMovie").click(searchMovie);
+        var movieTitle = $("#title");
+        var table = $("#results");
+        var tbody = $("#container");
+        var template = $("#template");
+
+        function searchMovie(){
+            var title = movieTitle.val();
+
+            var url = "http://www.myapifilms.com/imdb?title="+title+"&format=JSONP&aka=0&business=0&seasons=0&seasonYear=0&technical=0&filter=N&exactFilter=0&limit=3&forceYear=0&lang=en-us&actors=N&biography=0&trailer=1&uniqueName=0&filmography=0&bornDied=0&starSign=0&actorActress=0&actorTrivia=0&movieTrivia=0&awards=0&moviePhotos=N&movieVideos=N&similarMovies=0&adultSearch=0";
+
+            console.log(url);
+
+            $.ajax({
+                url: url,
+                dataType: "jsonp",
+                success : renderMoviesWithTemplate
+            })
+        }
+
+        function renderMoviesWithTemplate(movies){
+            console.log(movies);
+            tbody.empty();
+            for(var m in movies) {
+                var movie = movies[m];
+                console.log(movie);
+                var title = movie.title;
+                var plot = movie.simplePlot;
+                var urlPoster = movie.urlPoster;
+                console.log(urlPoster);
+                var imdbURL = movie.urlIMDB;
+                var tr = template.clone();
+
+                tr.find(".link").attr("href", imdbURL).html(title);
+                tr.find(".plot").html(plot);
+                tr.find(".imag").attr("src",urlPoster);
+                tbody.append(tr);
+            }
+
+
+        }
+
+        function renderMovies(movies){
+            alert("HI");
+            console.log(movies);
+            tbody.empty();
+
+            for(var m in movies){
+                var movie = movies[m];
+                console.log(movie);
+                var title = movie.title;
+                var plot = movie.simplePlot;
+                var urlPoster = movie.urlPoster;
+                var imdbURL = movie.urlIMDB;
+
+                var tr = $("<tr>" );
+                var titleTd = $("<a>").attr("href",imdbURL).html(title);
+                var titleLink = $("<td>").append(titleTd);
+
+                var titlePlot = $("<td>").append(plot);
+
+                var titleImage = $("<img>").attr("src",urlPoster);
+
+                var image  = $("<td>").append(titleImage);
+                tr.append(titleTd);
+
+                tr.append(titlePlot);
+
+                tr.append(image)
+
+                tbody.append(tr);
+            }
+        }
+    }
+})();
---
+++ public/Experiments/jQuery/soccer.js
@@ -0,0 +1,68 @@
+/**
+ * Created by Govinda on 10/8/2015.
+ */
+
+(function () {
+    $(init);
+
+    function init() {
+        var tbody = $("#container");
+        var row = $("#row");
+        var h3 = $("#matchday");
+        
+        $.ajax({
+            headers: { 'X-Auth-Token': '0918793d32054063ac52204e3d81735f' },
+            url: 'http://api.football-data.org/alpha/soccerseasons/398/leagueTable',
+            dataType: 'json',
+            type: 'GET',
+            success: renderStandings
+        })
+
+
+        function renderStandings(table) {
+            var md = "MatchDay " + table.matchday;
+            h3.find("md").html(md);
+            console.log(h3.html);
+            console.log(table);
+            tbody.empty();
+            var teams = table.standing;
+            console.log(teams);
+            for (var standing in teams) {
+                var crest;
+                var team = teams[standing];
+                var teamURL = team._links.team.href;
+                console.log(teamURL);
+                $.ajax({
+                    headers: { 'X-Auth-Token': '0918793d32054063ac52204e3d81735f' },
+                    url: teamURL,
+                    dataType: 'json',
+                    async : false,
+                    type: 'GET',
+                    success: function (teamInfo) {
+                        console.log(teamInfo);
+                        crest = teamInfo.crestUrl;
+                        console.log(teamInfo.crestUrl);
+                    }
+                })
+                var gd = team.goalDifference;
+                var gs = team.goals;
+                var ga = team.goalsAgainst;
+                var p = team.playedGames;
+                var points = team.points;
+                var pos = team.position;
+                var tn = team.teamName;
+                var tr = row.clone();
+                tr.find(".pos").html(pos);
+                tr.find(".team").html(tn);
+                tr.find(".pts").html(points);
+                tr.find(".p").html(p);
+                tr.find(".gs").html(gs);
+                tr.find(".ga").html(ga);
+                tr.find(".gd").html(gd);
+                tr.find(".crest").attr("src",crest);
+                tbody.append(tr);
+            }
+        }
+
+    }
+})();
